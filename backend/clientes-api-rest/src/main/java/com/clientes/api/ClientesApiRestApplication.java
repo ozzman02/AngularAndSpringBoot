74275262d2c6e8878520f04db38b8d1a963d18d6
Oscar Santamaria Venegas <osantamaria@Oscars-MacBook-Pro.local>
2023-04-04 20:27:34 -0600

    Structura inicial Java Backend

---
+++ backend/clientes-api-rest/src/main/java/com/clientes/api/ClientesApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.clientes.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesApiRestApplication.class, args);
+	}
+
+}
