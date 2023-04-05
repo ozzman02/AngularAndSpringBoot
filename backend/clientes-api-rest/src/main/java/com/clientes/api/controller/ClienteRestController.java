dfe04af79fdae341f8cc60f24789f5420c4d2a77
osantamaria <osantamaria@gmail.com>
2023-04-04 22:16:00 -0600

    habilitando CORS

--- backend/clientes-api-rest/src/main/java/com/clientes/api/controller/ClienteRestController.java
+++ backend/clientes-api-rest/src/main/java/com/clientes/api/controller/ClienteRestController.java
@@ -3,17 +3,19 @@ package com.clientes.api.controller;
 import com.clientes.api.domain.Cliente;
 import com.clientes.api.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
 
-    private ClienteService clienteService;
+    private final ClienteService clienteService;
 
     @Autowired
     public ClienteRestController(ClienteService clienteService) {
