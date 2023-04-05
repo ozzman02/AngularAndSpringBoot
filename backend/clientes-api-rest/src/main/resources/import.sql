cf68803a8a478421527debb41262d73d7478b17b
osantamaria <osantamaria@gmail.com>
2023-04-04 22:37:45 -0600

    get clientes desde angular

--- backend/clientes-api-rest/src/main/resources/import.sql
+++ backend/clientes-api-rest/src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO db_springboot_backend.clientes (nombre, apellido, email, create_at) VALUES('Oscar', 'Santamaría', 'osantmaria@gmail.com', '2023-04-04');
+INSERT INTO db_springboot_backend.clientes (nombre, apellido, email, create_at) VALUES('Oscar', 'Santamaría', 'osantamaria@gmail.com', '2023-04-04');
 INSERT INTO db_springboot_backend.clientes (nombre, apellido, email, create_at) VALUES('Luis', 'Venegas', 'lvenegas@gmail.com', '2023-04-04');
 INSERT INTO db_springboot_backend.clientes (nombre, apellido, email, create_at) VALUES('Karla', 'Solis', 'ksolis@gmail.com', '2023-04-04');
 INSERT INTO db_springboot_backend.clientes (nombre, apellido, email, create_at) VALUES('Melissa', 'Protti', 'mprotti@gmail.com', '2023-04-04');
--- frontend/clientes-app/src/app/clientes/cliente.service.ts
+++ frontend/clientes-app/src/app/clientes/cliente.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
+import { map, of } from 'rxjs';
 import { Observable } from 'rxjs';
 import { Cliente } from './cliente';
-import { CLIENTES } from './clientes.json';
+//import { CLIENTES } from './clientes.json';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ClienteService {
 
-  constructor() { }
+  private urlEndpoint: string = 'http://localhost:8080/api/clientes';
+
+  constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
+    //return of(CLIENTES);
+    //return this.http.get<Cliente[]>(this.urlEndpoint);
+    return this.http.get<Cliente[]>(this.urlEndpoint).pipe(
+      map(response => response as Cliente[])
+    );
   }
 
 }
