16f8c9dd0c0b6794585e397fcded92473dafce69
osantamaria <osantamaria@gmail.com>
2023-10-27 20:16:53 -0600

    frontend - sistema de facturas 5

--- frontend/clientes-app/src/app/facturas/facturas.component.ts
+++ frontend/clientes-app/src/app/facturas/facturas.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Factura } from './models/factura';
 import { ClienteService } from '../clientes/cliente.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FormControl } from '@angular/forms';
 import { Observable, flatMap, map } from 'rxjs';
 import { FacturaService } from './services/factura.service';
 import { Producto } from './models/producto';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { ItemFactura } from './models/item-factura';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-facturas',
@@ -24,7 +25,11 @@ export class FacturasComponent implements OnInit {
 
   productosFiltrados: Observable<Producto[]>;
 
-  constructor(private clienteService: ClienteService, private activatedRoute: ActivatedRoute, private facturaService: FacturaService) {}
+  constructor(
+    private clienteService: ClienteService, 
+    private activatedRoute: ActivatedRoute, 
+    private facturaService: FacturaService, 
+    private router: Router) {}
 
   ngOnInit(): void {
       this.activatedRoute.paramMap.subscribe(params => {
@@ -49,9 +54,15 @@ export class FacturasComponent implements OnInit {
   seleccionarProducto(event: MatAutocompleteSelectedEvent): void {
     let producto = event.option.value as Producto;
     console.log(producto);
-    let nuevoItem = new ItemFactura();
-    nuevoItem.producto = producto;
-    this.factura.items.push(nuevoItem);
+
+    if (this.existeItem(producto.id)) {
+      this.incrementaCantidad(producto.id);
+    } else {
+      let nuevoItem = new ItemFactura();
+      nuevoItem.producto = producto;
+      this.factura.items.push(nuevoItem);
+    }
+
     this.autoCompleteControl.setValue('');
     event.option.focus();
     event.option.deselect();
@@ -59,11 +70,45 @@ export class FacturasComponent implements OnInit {
 
   actualizarCantidad(id: number, event: any): void {
     let cantidad: number = event.target.value as number;
+    if (cantidad == 0) {
+      return this.eliminarItemFactura(id);
+    }
     this.factura.items = this.factura.items.map((item:ItemFactura) => {
       if (id === item.producto.id) {
         item.cantidad = cantidad;
       }
       return item;
+    });
+  }
+
+  existeItem(id: number): boolean {
+    let existe = false;
+    this.factura.items.forEach((item: ItemFactura) => {
+      if (id === item.producto.id) {
+        existe = true;
+      }
+    });
+    return existe;
+  } 
+  
+  incrementaCantidad(id: number): void {
+    this.factura.items = this.factura.items.map((item:ItemFactura) => {
+      if (id === item.producto.id) {
+        ++item.cantidad;
+      }
+      return item;
+    });
+  }
+
+  eliminarItemFactura(id: number): void {
+    this.factura.items = this.factura.items.filter((item: ItemFactura) => id !== item.producto.id);
+  }
+
+  create(): void {
+    console.log(this.factura);
+    this.facturaService.create(this.factura).subscribe(factura => {
+      swal(this.titulo, `Factura ${factura.descripcion} creada con éxito!`, 'success');
+      this.router.navigate(['/clientes']);
     });
   }
 
--- frontend/clientes-app/src/app/facturas/services/factura.service.ts
+++ frontend/clientes-app/src/app/facturas/services/factura.service.ts
@@ -25,5 +25,9 @@ export class FacturaService {
     return this.http.get<Producto[]>(`${this.urlEndPoint}/filtrar-productos/${term}`);
   }
 
+  create(factura: Factura): Observable<Factura> {
+    return this.http.post<Factura>(this.urlEndPoint, factura);
+  }
+
 
 }
