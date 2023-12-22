2c482847b62064caf59ad91282dc5b79e4e4e342
osantamaria <osantamaria@gmail.com>
2023-12-21 23:24:01 -0600

    chat-frontend 2

--- chat-project/frontend/chat-app/src/app/chat/chat.component.ts
+++ chat-project/frontend/chat-app/src/app/chat/chat.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Client } from '@stomp/stompjs';
 import * as SockJS from 'sockjs-client';
+import { Mensaje } from './models/mensaje';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
@@ -11,7 +13,19 @@ export class ChatComponent implements OnInit {
 
   private client: Client = new Client();
 
-  constructor() {}
+  conectado: boolean = false;
+
+  mensaje: Mensaje = new Mensaje();
+
+  mensajes: Mensaje[] = [];
+
+  escribiendo: string = '';
+
+  clienteId: string = '';
+
+  constructor() {
+    this.clienteId = 'id-' + new Date().getUTCMilliseconds() + '-' + Math.random().toString(36).substring(2);
+  }
 
   ngOnInit(): void {
     this.client.webSocketFactory = () => {
@@ -19,9 +33,62 @@ export class ChatComponent implements OnInit {
     }
     this.client.onConnect = (frame) => {
       console.log('Conectados: ' + this.client.connected + ' : ' + frame);
-    } 
-    this.client.activate();
+      this.conectado = true;
+      
+      this.client.subscribe('/chat/mensaje', e => {
+        let mensaje: Mensaje = JSON.parse(e.body) as Mensaje;
+        if (!this.mensaje.color && mensaje.tipo == 'NUEVO_USUARIO' && this.mensaje.username == mensaje.username) {
+          this.mensaje.color = mensaje.color;
+        }
+        this.mensajes.push(mensaje);
+        console.log(mensaje);
+      });
 
+      this.client.subscribe('/chat/escribiendo', e => {
+        this.escribiendo = e.body;
+        setTimeout(() => this.escribiendo = '', 3000)
+      });
+
+      console.log(this.clienteId);
+
+      this.client.subscribe('/chat/historial/' + this.clienteId, e => {
+        const historial = JSON.parse(e.body) as Mensaje[];
+        this.mensajes = historial.map(m => {
+          m.fecha = new Date(m.fecha);
+          return m;
+        }).reverse();
+      });
+
+      this.client.publish({destination: '/app/historial', body: this.clienteId});
+     
+      this.mensaje.tipo = 'NUEVO_USUARIO';
+      this.client.publish({destination: '/app/mensaje', body: JSON.stringify(this.mensaje)});
+    }
+
+    this.client.onDisconnect = (frame) => {
+      console.log('Desconectados: ' + !this.client.connected + ' : ' + frame);
+      this.conectado = false;
+      this.mensaje = new Mensaje();
+      this.mensajes = [];
+    }
   }
+
+  conectar(): void {
+    this.client.activate();
+  }
+
+  desconectar(): void {
+    this.client.deactivate();
+  }
+
+  enviarMensaje(): void {
+    this.mensaje.tipo = 'MENSAJE';
+    this.client.publish({destination: '/app/mensaje', body: JSON.stringify(this.mensaje)});
+    this.mensaje.texto = '';
+  }
+
+  escribiendoEvento(): void {
+    this.client.publish({destination: '/app/escribiendo', body: this.mensaje.username});
+  } 
 
 }
